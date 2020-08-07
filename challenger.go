a5f2f8368505e7eb23d2b6c16994cd1a62306709
Oliver Gugger <gugger@gmail.com>
2020-08-07 17:33:09 +0200

    aperture: use lnd client through interface

--- challenger.go
+++ challenger.go
@@ -14,16 +14,34 @@ import (
 	"github.com/lightninglabs/lndclient"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lntypes"
+	"google.golang.org/grpc"
 )
 
 // InvoiceRequestGenerator is a function type that returns a new request for the
 // lnrpc.AddInvoice call.
 type InvoiceRequestGenerator func(price int64) (*lnrpc.Invoice, error)
 
+// InvoiceClient is an interface that only implements part of a full lnd client,
+// namely the part around the invoices we need for the challenger to work.
+type InvoiceClient interface {
+	// ListInvoices returns a paginated list of all invoices known to lnd.
+	ListInvoices(ctx context.Context, in *lnrpc.ListInvoiceRequest,
+		opts ...grpc.CallOption) (*lnrpc.ListInvoiceResponse, error)
+
+	// SubscribeInvoices subscribes to updates on invoices.
+	SubscribeInvoices(ctx context.Context, in *lnrpc.InvoiceSubscription,
+		opts ...grpc.CallOption) (
+		lnrpc.Lightning_SubscribeInvoicesClient, error)
+
+	// AddInvoice adds a new invoice to lnd.
+	AddInvoice(ctx context.Context, in *lnrpc.Invoice,
+		opts ...grpc.CallOption) (*lnrpc.AddInvoiceResponse, error)
+}
+
 // LndChallenger is a challenger that uses an lnd backend to create new LSAT
 // payment challenges.
 type LndChallenger struct {
-	client        lnrpc.LightningClient
+	client        InvoiceClient
 	genInvoiceReq InvoiceRequestGenerator
 
 	invoiceStates  map[lntypes.Hash]lnrpc.Invoice_InvoiceState
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/lightningnetwork/lnd/cert v1.0.2
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/stretchr/testify v1.5.1
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20200122045848-3419fae592fc // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.uber.org/zap v1.15.0 // indirect
